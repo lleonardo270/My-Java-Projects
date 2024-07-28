bfbb9c42b29c186580c2120c823d29acf5811e6f
Lucas Brandao <lucasleo270@gmail.com>
2024-07-28 16:13:18 -0300

    Nome
    Método implementado com variável de nome declarado, e concatenado.

--- Javatestevscode/src/MinhaClasse.java
+++ Javatestevscode/src/MinhaClasse.java
@@ -1,8 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "Leonardo";
 
-        System.out.print ("Olá Turma, sejam bem vindos!");
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
     }
-    
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método "+ primeiroNome.concat(" ").concat(segundoNome);
+
+    }
 }
